b8a168c4cab86ff39617a3cf5459e6364bc6b59e
zermello <shonsanthosh00@gmail.com>
2026-08-26 21:46:47 +0530

    Implement addition in random class
    Added a random class with an __add__ method to enable addition of two instances.

--- OOPS/inheritance_ad.py
+++ OOPS/inheritance_ad.py
@@ -33,3 +33,16 @@ print(f"{robo.abc} and {robo.id}")
 #     return self + other
 
 # __add__(1, 2)
+
+class random:
+    def __init__(self, num):
+        self.num = num
+
+    def __add__(self, other):
+        return self.num + other.num
+
+num1 = random(2)
+num2 = random(3)
+
+huh = random.__add__(num1, num2)
+print(huh)
